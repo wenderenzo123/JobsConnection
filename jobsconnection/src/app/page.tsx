b17abd85e2e22f3188c7a3616907b1b8ccc0f6bd
Wender Enzo <wenderenzo2014@outlook.com>
2023-02-01 22:50:40 -0300

    fix. ajustes iniciais

--- jobsconnection/src/app/page.tsx
+++ jobsconnection/src/app/page.tsx
@@ -1,9 +1,9 @@
 export default function Home() {
   return (
     <div>
-      <h1 className="text-3xl font-bold">
-      Hello world!
-    </h1>
+      <h1 className="text-3xl font-bold text-black">
+        Jobs Connection
+      </h1>
     </div>
   )
 }
